df346bd3574878f23d417ea6601558761e72f434
li3317 <46685982+li3317@users.noreply.github.com>
2020-12-02 17:08:47 -0500

    add edge smoothing

--- cartoon-image/transfer_frame.py
+++ cartoon-image/transfer_frame.py
@@ -215,6 +215,47 @@ def style_transfer():
 #=========================<Main>================================================
 
 
+# code for smoothing out the style image
+
+# Reference: https://github.com/znxlwm/pytorch-CartoonGAN
+def smoothout(dataset_name, img_size):
+    check_folder('./dataset/{}/{}'.format(dataset_name, 'trainB_smooth'))
+    file_list = glob('./dataset/{}/{}/*.*'.format(dataset_name, 'trainB'))
+    save_dir = './dataset/{}/trainB_smooth'.format(dataset_name)
+
+    kernel_size = 5
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    gauss = cv2.getGaussianKernel(kernel_size, 0)
+    gauss = gauss * gauss.transpose(1, 0)
+
+    for f in tqdm(file_list):
+        file_name = os.path.basename(f)
+
+        bgr_img = cv2.imread(f)
+        gray_img = cv2.imread(f, 0)
+
+        bgr_img = cv2.resize(bgr_img, (img_size, img_size))
+        pad_img = np.pad(bgr_img, ((2, 2), (2, 2), (0, 0)), mode='reflect')
+        gray_img = cv2.resize(gray_img, (img_size, img_size))
+
+        edges = cv2.Canny(gray_img, 100, 200)
+        dilation = cv2.dilate(edges, kernel)
+
+        gauss_img = np.copy(bgr_img)
+        idx = np.where(dilation != 0)
+        for i in range(np.sum(dilation != 0)):
+            gauss_img[idx[0][i], idx[1][i], 0] = np.sum(
+                np.multiply(pad_img[idx[0][i]:idx[0][i] + kernel_size, idx[1][i]:idx[1][i] + kernel_size, 0], gauss))
+            gauss_img[idx[0][i], idx[1][i], 1] = np.sum(
+                np.multiply(pad_img[idx[0][i]:idx[0][i] + kernel_size, idx[1][i]:idx[1][i] + kernel_size, 1], gauss))
+            gauss_img[idx[0][i], idx[1][i], 2] = np.sum(
+                np.multiply(pad_img[idx[0][i]:idx[0][i] + kernel_size, idx[1][i]:idx[1][i] + kernel_size, 2], gauss))
+
+        cv2.imwrite(os.path.join(save_dir, file_name), gauss_img)
+
+
+
+
 def main():
     K.clear_session()
     print("Starting style transfer program.")
@@ -223,6 +264,8 @@ def main():
     # global currentPath
     # currentPath = 'frame629.jpg'
     # style_transfer()
+    
+    smoothout(STYLE_IMG_PATH, 256)
 
     global currentPath
     for path in pathlist:
